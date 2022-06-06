b16665b9e6ebdfaecfe987afcf8dea6a69eb190b
TivaniMabunda <95340204+TivaniMabunda@users.noreply.github.com>
2022-06-06 04:49:28 +0200

    distructuring

---
+++ distructuring.js
@@ -0,0 +1,41 @@
+const routine = {
+    title1: "Quiet Time",
+    title2 : "Study",
+    title3 : "Go Jogging",
+    title4 : "Eat Breakfast",
+    description1 : "",
+    description2 : "",
+    decription3 : "This is going to help to reach my goals and my life to the fullest",
+    decription4 : "",
+    date1 : "05/02/2020",
+    date2 : "01/02/2020",
+    date3 : "tomorrow",
+    date4 : "today",
+    time1 : "08:12",
+    time2 : "13:15",
+    time3 : "12:36",
+    time4 : "13:25",
+    completed1 : false,
+    completed2 : true,
+    completed3 : false,
+    completed4 : true,
+    priority1 : "red",
+    priority2 : "yellow",
+    priority3 : "black",
+    priority4 : "white",
+    tags1 : ["Personal", "Work", "School"],
+    tags2 : ["Personal", "School", "Diary Entry"],
+    tags3 : ["Content Creation", "Personal"],
+    tags4 : ["Personal"]
+  }
+  const {title1, description1, date1, time1, completed1, priority1, tags1} = routine;
+  console.log(title1 + ',' + description1 + ',' + date1 + ',' + completed1 + ',' + priority1 + ',' + tags1);
+
+  const {title2, description2, date2, time2, completed2, priority2, tags2} = routine;
+  console.log(title2 + ',' + description2 + ',' + date2 + ',' + completed2 + ',' + priority2 + ',' + tags2);
+
+  const {title3, description3, date3, time3, completed3, priority3, tags3} = routine;
+  console.log(title3 + ',' + description3 + ',' + date3 + ',' + completed3 + ',' + priority3 + ',' + tags3);
+
+  const {title4, description4, date4, time4, completed4, priority4, tags4} = routine;
+  console.log(title4 + ',' + description4 + ',' + date4 + ',' + completed4 + ',' + priority4 + ',' + tags4);
